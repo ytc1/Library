2b5cf87978f98a33c09a3ffe2a452a3d702ea954
Yechiel Cohen <ytc92@yahoo.com>
2016-05-05 10:45:53 -0400

    Added to Admin

--- Library/Controllers/AdminController.cs
+++ Library/Controllers/AdminController.cs
@@ -30,6 +30,34 @@ namespace Library.Controllers
             FormsAuthentication.SetAuthCookie(admin.FirstName, true);         
             return RedirectToAction("Main");
         }
+        [HttpPost]
+        public ActionResult AddBook(string name, string authorFirst, string authorLast , string description)
+        {
+            DataBase Db = new DataBase(connectionString);
+            bool exists = Db.CheckAuthor(authorFirst, authorLast);
+            if(exists)
+            {
+                Author a = Db.GetAuthor(authorFirst, authorLast);
+                Book b = new Book();
+                b.AuthorId = a.Id;
+                b.Name = name;
+                b.Description = description;
+                Db.AddBook(b);
+            }
+            else
+            {
+                Author a = new Author();
+                a.FirstName = authorFirst;
+                a.LastName = authorLast;
+                Db.AddAuthor(a);
+                Book b = new Book();
+                b.AuthorId = a.Id;
+                b.Name = name;
+                b.Description = description;
+                Db.AddBook(b);
+            }
+            return RedirectToAction("Main");
+        }
 
     }
 }
